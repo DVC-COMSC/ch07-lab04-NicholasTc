5f8cc1b3e5940ef8dd739445785c4d1bbe4cb252
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-08 20:13:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 
 # ******************************
 # Make your Code
-# ******************************
 import random
 
 numbers1 = []
 numbers2 = []
-result = []
+sumList = []
+
 for i in range(10):
-	numbers1.append(random.randint(0,100))
-	numbers2.append(random.randint(0,100))
-print (numbers1)
-print (numbers2)
+    num1 = random.randint(0,10)
+    num2 = random.randint(0,10)
+    numSum = num1 + num2
+
+    numbers1.append(num1)
+    numbers2.append(num2)
+    sumList.append(numSum)
+
+print(sumList)
+
+
+
+
+
